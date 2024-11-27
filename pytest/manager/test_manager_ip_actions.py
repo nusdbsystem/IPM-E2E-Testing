13b3bd393599efe418c23552e6f0d11b3583d817
daoducanhc <daoducanhc@gmail.com>
2024-11-27 11:05:03 +0800

    Add scripts Inventor/Manager IP actions

--- pytest/manager/test_manager_ip_actions.py
+++ pytest/manager/test_manager_ip_actions.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Page, expect
 
 def test_example(page: Page) -> None:
     page.goto("http://137.132.92.226:4001/")
-    page.get_by_placeholder("Email address*").fill("ducanh@nus.edu.sg")
+    page.get_by_placeholder("Email address*").fill("zhongkai@nus.edu.sg")
     page.get_by_placeholder("Password*").fill("123")
     page.get_by_role("button", name="Login").click()
 
@@ -14,10 +14,6 @@ def test_example(page: Page) -> None:
     page.get_by_placeholder("Enter a title for this").click()
     page.get_by_placeholder("Enter a title for this").fill("test")
     page.get_by_role("button", name="Create").click()
-
-    # import IP from paper
-    page.get_by_role("banner").get_by_role("link").click()
-    page.get_by_role("link", name="Inventions").click()
 
     # import IP from Patent ID
     page.get_by_role("banner").get_by_role("link").click()
@@ -36,9 +32,26 @@ def test_example(page: Page) -> None:
     # expect(alert).to_be_visible({ "timeout": 15000 })
     expect(alert).to_have_text("File imported!", timeout=15000)
 
-    # submit IP to manager
-    page.get_by_role("button", name="Submit").click()
-    page.locator("div").filter(has_text=re.compile(r"^Select manager$")).nth(1).click()
-    page.get_by_text("IP8Value成果转化主管 (ip8value_test").click()
-    page.get_by_role("button", name="Add").click()
+    # generate tech offer
+    page.locator("a").filter(has_text="Sections11").click()
+    page.get_by_role("button", name="Generate Tech offer").click()
+    page.get_by_placeholder("Select section titles").click()
+    page.get_by_role("option", name="Abstract Word Count:").click()
+    page.get_by_role("option", name="SUMMARY Word Count:").click()
+    page.get_by_role("option", name="DETAILED DESCRIPTION Word Count:").click()
+    page.get_by_label("Close").click()
+    page.get_by_placeholder("Select template").click()
+    page.get_by_text("Default tech offer [English").click()
+    page.get_by_role("button", name="Confirm").click()
     expect(page.get_by_role("alert")).not_to_be_visible()
+    # expect "Content Generating" to be visible
+    expect(page.get_by_text("Content Generating")).to_be_visible()
+
+    # transfer IP ownership
+    page.get_by_text("Advanced").click()
+    page.get_by_role("button", name="Transfer").click()
+    page.locator("div").filter(has_text=re.compile(r"^Select a user$")).nth(1).click()
+    page.get_by_text("IP8Value成果转化主管 (ip8value_test").click()
+    page.get_by_role("button", name="Transfer").click()
+    expect(alert).to_be_visible()
+    expect(alert).to_have_text("IP ownership has been transferred.")
